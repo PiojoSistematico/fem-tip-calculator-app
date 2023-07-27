683554eaa70f9d53a1e1485625871c9a9531b5ad
Jorge Saa <piojo.sistematico@gmail.com>
2023-07-26 20:41:49 -0400

    basic html and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <section className="title">
+        <span>spli</span>
+        <span>tter</span>
+      </section>
+      <form>
+        <div className="input-section">
+          <label htmlFor="bill">Bill</label>
+          <input type="number" name="bill" placeholder="0.0" />
+        </div>
+        <div className="btn-section">
+          <label htmlFor="tip">Select Tip %</label>
+          <div className="btn-grid">
+            <button className="btn-tip">5%</button>
+            <button className="btn-tip">10%</button>
+            <button className="btn-tip">15%</button>
+            <button className="btn-tip">25%</button>
+            <button className="btn-tip">50%</button>
+
+            <input type="number" name="custom-tip" placeholder="Custom" />
+          </div>
+        </div>
+
+        <div className="input-section">
+          <label htmlFor="people">Number of People</label>
+          <input type="number" name="people" placeholder="1" />
+        </div>
+
+        <section className="result-section">
+          <section className="flex-between">
+            <div className="grid-two-rows">
+              <span className="tip-title">Tip Amount</span>
+              <span>/ person</span>
+            </div>
+            <span className="tip">XXX</span>
+          </section>
+          <section className="flex-between">
+            <div className="grid-two-rows">
+              <span className="tip-title">Total</span>
+              <span>/ person</span>
+            </div>
+            <span className="tip">XXX</span>
+          </section>
+          <button className="btn-reset">Reset</button>
+        </section>
+      </form>
+    </main>
+  );
 }
 
-export default App
+export default App;
